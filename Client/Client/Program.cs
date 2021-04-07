6dbf6276ac4d6176f8d782322fa65ec8007edb18
Youssef kafa <48362996+YoussefKafa@users.noreply.github.com>
2021-04-08 01:30:55 +0300

    build client

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using General;
 namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            HttpChannel httpChannel = new HttpChannel();
+            ChannelServices.RegisterChannel(httpChannel, false);
+            ICustomerManager proxy = (ICustomerManager)Activator.GetObject(typeof(ICustomerManager), "http://localhost:5000/MyService.soap");
+            Console.WriteLine("proxy is ready to use");
+            //proxy.findAll();
+            Console.ReadKey();
         }
     }
 }
